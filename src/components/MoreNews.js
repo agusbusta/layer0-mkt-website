18a59bb4fbace9a221ac06beb5875f7c9a71040a
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-03-22 16:27:51 -0300

    first commit

---
+++ src/components/MoreNews.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ethereum from "../assets/ethereum.png";
+import bitcoin from "../assets/bitcoin.png";
+import layer1 from "../assets/layer1.png";
+
+function MoreNews() {
+  return (
+    <>
+      <br />
+      <div className="aside-content">
+        <h2>More News</h2>
+        <hr />
+        <br />
+        <ul className="website-list">
+          <div className="website-group">
+            <li className="website-li">
+              <a href="#">
+                <img
+                  src={ethereum}
+                  className="otherwebsiteimg"
+                  alt="Ethereum"
+                />
+                <p className="textCardMoreWebsites">Ethereum</p>
+              </a>
+            </li>
+            <li className="website-li">
+              <a href="#">
+                <img src={bitcoin} className="otherwebsiteimg" alt="Bitcoin" />
+                <p className="textCardMoreWebsites"> Bitcoin</p>
+              </a>
+            </li>
+          </div>
+          <div className="website-entire">
+            <li className="website-li layer1div">
+              <a href="#">
+                <img src={layer1} className="layer1logo" alt="Layer 1 (LMC)" />
+                <p className="textCardMoreWebsites">Layer 1</p>
+              </a>
+            </li>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default MoreNews;
